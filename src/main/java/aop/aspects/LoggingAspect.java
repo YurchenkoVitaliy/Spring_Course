e805f3c9a6d6204fe0dbea8df967c4bb62fa3e19
Vitaliy Yurchenko <yurchenkovitaliy92@gmail.com>
2024-01-14 19:37:22 +0600

    30. Порядок выполнения Aspect-ов

---
+++ src/main/java/aop/aspects/LoggingAspect.java
@@ -0,0 +1,69 @@
+package aop.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Order(1)
+public class LoggingAspect {
+
+//    @Pointcut("execution(* aop.UniLibrary.*(..))")
+//    private void allMethodsFromUniLibrary(){
+//    }
+//
+//    @Pointcut("execution(public void aop.UniLibrary.returnMagazine())")
+//    private void returnMagazineFromUniLibrary(){
+//    }
+//
+//    @Pointcut("allMethodsFromUniLibrary() && !returnMagazineFromUniLibrary()")
+//    private void allMethodsExceptReturnMagazineFromUniLibrary(){
+//    }
+//
+//    @Before("allMethodsExceptReturnMagazineFromUniLibrary()")
+//    public void beforeAllMethodsExceptReturnMagazineAdvice(){
+//        System.out.println("beforeAllMethodsExceptReturnMagazineAdvice: Log #10");
+//    }
+
+
+//    @Pointcut("execution(* aop.UniLibrary.get*())")
+//    private void allGetMethodsFromUniLibrary(){}
+//
+//    @Pointcut("execution(* aop.UniLibrary.return*())")
+//    private void allReturnMethodsFromUniLibrary(){}
+//
+//    @Pointcut("allGetMethodsFromUniLibrary() || allReturnMethodsFromUniLibrary()")
+//    private void allGetAndReturnMethodsFromUniLibrary(){}
+//
+//
+//    @Before("allGetMethodsFromUniLibrary()")
+//    public void beforeGetLoggingAdvice(){
+//        System.out.println("beforeGetLoggingAdvice: writing Log #1");
+//    }
+//
+//    @Before("allReturnMethodsFromUniLibrary()")
+//    public void beforeReturnLoggingAdvice(){
+//        System.out.println("beforeReturnLoggingAdvice: writing Log #2");
+//    }
+//
+//    @Before("allGetAndReturnMethodsFromUniLibrary()")
+//    public void allGetAndReturnLoggingAdvice(){
+//        System.out.println("beforeGetAndReturnLoggingAdvice: writing Log #3");
+//    }
+
+
+
+
+
+
+
+
+    @Before("aop.aspects.MyPointcuts.allGetMethods()")
+    public void beforeGetLoggingAdvice() {
+        System.out.println("beforeGetLoggingAdvice: логирование попытки получить книгу/журнал");
+    }
+
+
+}
